28f94a64944a098e7800757b65aad063a97a43a7
Lucas Lemos <balahallslemos@gmail.com>
2023-10-24 11:13:25 -0300

    Menssagem de alerta adicionado

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  isAlertOpen = false;
+  public alertButtons = ['OK'];
 
-  constructor() {}
-
+  setOpen(isOpen: boolean) {
+    this.isAlertOpen = isOpen;
+  }
 }
